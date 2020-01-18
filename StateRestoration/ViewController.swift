76b4e088d60ac61714709845dec4bc00bfb1d307
Sanjay <sanjayp@g.clemson.edu>
2020-01-18 12:32:42 -0500

    Initial Commit

---
+++ StateRestoration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StateRestoration
+//
+//  Created by Sanjay Patil on 1/18/20.
+//  Copyright © 2020 Sanjay Patil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
